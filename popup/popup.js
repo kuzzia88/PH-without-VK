b9f99042549c789502547526d78da87b1356150d
kuzzia. <pspjmuzz@gmail.com>
2025-09-27 16:31:09 +0300

    Add files via upload

---
+++ popup/popup.js
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Расширение для браузера: ph-without-vk
+ * Copyright (c) 2025 kuzzia88
+ * MIT License
+ */
+
+document.getElementById('by').addEventListener('click', async () => {
+    window.open('https://github.com/kuzzia88', '_blank');
+});
